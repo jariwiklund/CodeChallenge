378d3cc7f9cf7121cfeae69ce33602b2b486dc76
Jari Wiklund <jari.wiklund@gmail.com>
2016-11-16 22:01:28 +0100

    Last commit for now

--- src/Controller.php
+++ src/Controller.php
@@ -1,18 +1,21 @@
 <?php
 class Controller {
     
+    private $request;
     public function __construct(\Psr\Http\Message\RequestInterface $request){
         //todo: setup routing...
+        $this->request = $request;
     }
     
     public function CreateBooking(){
-        
+        $validator = new \CodeChallenge\Validators\BookingJsonValidator();
+        $validator->validateData($this->request->getBody());
     }
     
     public function AddNotificationToBooking(\CodeChallenge\Models\Booking $booking){
-        
+        $validator = new \CodeChallenge\Validators\NotificationJsonValidator();
+        $validator->validateData($this->request->getBody());
+        //todo:persist
     }
-    
-    
     
 }
